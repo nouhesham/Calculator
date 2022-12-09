70bd7a9a0f01df0aa7b50927e1951c64a7fa5de9
Nourhan <nourhanhesham263@gmail.com>
2022-12-09 23:19:44 +0200

    adding queryselectors

---
+++ javaS.js
@@ -0,0 +1,5 @@
+"strict";
+const operations = document.querySelectorAll("[data-operations]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationsButton = document.querySelectorAll("[data-operations]");
+const equalButton
