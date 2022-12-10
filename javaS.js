0eae1e0624038d8d05a91304243faaec013e5fad
Nourhan <nourhanhesham263@gmail.com>
2022-12-10 23:55:26 +0200

    adding new documentslector

--- javaS.js
+++ javaS.js
@@ -2,4 +2,10 @@
 const operations = document.querySelectorAll("[data-operations]");
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationsButton = document.querySelectorAll("[data-operations]");
-const equalButton
+const equalButton = document.querySelector("[data-equals");
+const allclearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousButton = document.querySelector("[dat-previous-operand]");
+const currentoperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
