6e6a9d8649ef596d5466c1945e9fb758d338134c
Nourhan <nourhanhesham263@gmail.com>
2022-12-12 22:54:47 +0200

    adding new class

--- javaS.js
+++ javaS.js
@@ -1,11 +1,36 @@
 "strict";
+
+class calculator {
+  constructor(previousoperandTextElement, currentoperandTextElement) {
+    this.previousoperandTextElement = previousoperandTextElement;
+    this.currentoperandTextElement = currentoperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentoperand = "";
+    this.previousoperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+  appendNumber(number) {}
+
+  chooseOperations(operation) {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
 const operations = document.querySelectorAll("[data-operations]");
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationsButton = document.querySelectorAll("[data-operations]");
 const equalButton = document.querySelector("[data-equals");
 const allclearButton = document.querySelector("[data-all-clear]");
 const deleteButton = document.querySelector("[data-delete]");
-const previousButton = document.querySelector("[dat-previous-operand]");
+const previousoperandTextElement = document.querySelector(
+  "[dat-previous-operand]"
+);
 const currentoperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
